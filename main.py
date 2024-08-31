65b19faa1c246a2f76584f6a8c21d77508001a42
ajay-rs <ajayrav516@gmail.com>
2024-08-31 18:22:07 +0530

    file to folder changing photos and file

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+choco=15
+w=15
+while w>=3:
+    w=w-3
+    w=w+1
+    choco=choco+1
+print(choco)
+'''
+
+hours=10
+t=60
+c=1
+while c<=12:
+    hours=hours+1
+    t=t-5
+    c=c+1
+print(hours,t)
+
+
+
+
+
+
+
+
